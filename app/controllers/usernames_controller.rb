3013f9266104c53f780f167c7fe8fd3b3f1440c7
Daniel Florczak <danyflorczak@gmail.com>
2023-01-09 22:09:23 +0100

    adding flash messages to usernames controller

--- app/controllers/usernames_controller.rb
+++ app/controllers/usernames_controller.rb
@@ -7,13 +7,30 @@ class UsernamesController < ApplicationController
   def new; end
 
   def update
-    current_user.update(username_params)
-    redirect_to dashboard_path
+    if invalid_username?
+      flash[:alert] = "Please Set a Username"
+      redirect_to new_username_path
+    else
+      update_username
+      redirect_to dashboard_path
+    end
   end
 
   private
 
   def username_params
     params.require(:user).permit(:username)
+  end
+
+  def invalid_username?
+    username_params[:username].blank?
+  end
+
+  def update_username
+    if current_user.update(username_params)
+      flash[:notice] = "Username successfully updated"
+    else
+      flash[:alert] = current_user.errors.full_messages.join(", ")
+    end
   end
 end
