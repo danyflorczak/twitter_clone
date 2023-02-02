57bd4b686657e330ec6525db92a4b23a9f61d45e
Daniel Florczak <danyflorczak@gmail.com>
2023-02-02 14:02:16 +0100

    rubocop fixes

--- db/migrate/20230202084104_add_display_name_to_users.rb
+++ db/migrate/20230202084104_add_display_name_to_users.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class AddDisplayNameToUsers < ActiveRecord::Migration[7.0]
   def up
     add_column :users, :display_name, :string
 
     query = <<~SQL
-    update users
-    set display_name = username
-    where display_name is null
+      update users
+      set display_name = username
+      where display_name is null
     SQL
     ActiveRecord::Base.connection.execute(query)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,22 +7,20 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:username).case_insensitive }
 
   describe "#set_display_name" do
-    context "when display_name is set" do 
+    context "when display_name is set" do
       it "does not change the display_name" do
         user = build(:user, username: "bombel", display_name: "daniel florczak")
-        user.save 
+        user.save
         expect(user.reload.display_name).to eq("daniel florczak")
       end
     end
 
-    context "when display_name is not set" do 
+    context "when display_name is not set" do
       it "humanizes the previously set username" do
         user = build(:user, username: "bombel", display_name: nil)
         user.save
         expect(user.reload.display_name).to eq("Bombel")
       end
-
     end
-
   end
 end
