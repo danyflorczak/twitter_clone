13b9d29203e3c6338c6087d9a01c4a6e91220275
Daniel Florczak <danyflorczak@gmail.com>
2023-02-05 00:39:33 +0100

    creating tweet presenter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @tweets = Tweet.order(created_at: :desc).map { |tweet| TweetPresenter.new(tweet) }
+  end
 end
