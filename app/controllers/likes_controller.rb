5d00a885aef748aa16e1440c2ecd4b977a1e92a0
Daniel Florczak <danyflorczak@gmail.com>
2023-02-06 15:56:45 +0100

    add create method for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,22 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
-  def create; end
+  def create
+    current_user.likes.create(tweet: tweet)
+
+    if @tweet.save
+      respond_to do |format|
+        format.html { redirect_to dashboard_path }
+        format.turbo_stream
+      end
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def tweet
+    @tweet ||= Tweet.find(params[:tweet_id])
+  end 
 end
