dcaca81c516a8edca5eea209303228eda20e73ba
Daniel Florczak <danyflorczak@gmail.com>
2023-02-26 22:44:16 +0100

    creating RetweetsController and turbo stream actions create and destroy actions

---
+++ app/controllers/retweets_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class RetweetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @retweet = current_user.retweets.create(tweet:)
+
+    return unless @tweet.save
+
+    respond_to do |format|
+      format.html { redirect_to dashboard_path }
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    @retweet = tweet.retweets.find(params[:id])
+    @retweet.destroy
+    respond_to do |format|
+      format.html { redirect_to dashboard_path }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def tweet
+    @tweet ||= Tweet.find(params[:tweet_id])
+  end
+end
