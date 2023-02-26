c40038b0258e81dd7150f9de18749a674469bdf7
Daniel Florczak <danyflorczak@gmail.com>
2023-02-26 23:09:29 +0100

    adding specs for retweets controller

---
+++ spec/requests/retweets_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Retweets", type: :request do
+  let(:user) { create(:user) }
+  let(:tweet) { create(:tweet) }
+
+  before { sign_in user }
+
+  describe "POST create" do
+    it "create a new retweet" do
+      expect do
+        post tweet_retweets_path(tweet)
+      end.to change { Retweet.count }.by(1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+  describe "DELETE destroy" do
+    it "deletes a retweet" do
+      retweet = create(:retweet, user:, tweet:)
+      expect do
+        delete tweet_retweet_path(tweet, retweet)
+      end.to change { Retweet.count }.by(-1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+end
