ea09b03af89f2fcde7bcc28f2737a0c9560e0b43
Daniel Florczak <danyflorczak@gmail.com>
2023-03-02 17:51:44 +0100

    add: views count to tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_163236) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_164243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_163236) do
     t.datetime "updated_at", null: false
     t.integer "likes_count", default: 0
     t.integer "retweets_count", default: 0, null: false
+    t.integer "views_count", default: 0, null: false
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
