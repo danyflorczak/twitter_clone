a8cd62aa8c5b8030c993486f9e6e0ff84c567dc5
Daniel Florczak <danyflorczak@gmail.com>
2023-03-02 17:44:31 +0100

    test: has many associations in tweet and user and view specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
   it { should have_many(:bookmarked_tweets).through(:bookmarks).source(:tweet) }
   it { should have_many(:retweets).dependent(:destroy) }
   it { should have_many(:retweeted_tweets).through(:retweets).source(:tweet) }
+  it { should have_many(:views) }
+  it { should have_many(:viewd_tweets).through(:views) }
 
   describe "#set_display_name" do
     context "when display_name is set" do
