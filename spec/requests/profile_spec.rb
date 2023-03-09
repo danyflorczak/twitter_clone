66c343a14c86026815bc82513393de22363c13e7
Daniel Florczak <danyflorczak@gmail.com>
2023-03-09 14:33:19 +0100

    test: updating user profile

---
+++ spec/requests/profile_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Profile", type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+  describe "GET show" do
+    it "succeeds" do
+      get profile_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT update" do
+    it "updates the profile" do 
+      expect do
+        put profile_path, params: {
+          user: {
+            bio: "new bio"
+          }
+        }
+      end.to change { user.reload.bio }.from(nil).to("new bio")
+      expect(response).to redirect_to(profile_path)
+    end
+  end
+end
