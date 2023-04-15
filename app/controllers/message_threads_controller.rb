d9c8e1e78b746842f503f011f1b4c7115bd1576e
Daniel Florczak <danyflorczak@gmail.com>
2023-04-15 15:20:51 +0200

    add: starting message thread when message button is clicked

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -1,6 +1,9 @@
 class MessageThreadsController < ApplicationController
   before_action :authenticate_user!
-  def index
 
+  def index
+    if params[:user_id].present?
+      @new_message_thread_user = User.find(params[:user_id])
+    end
   end
 end
